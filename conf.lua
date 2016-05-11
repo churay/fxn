74306d58c89bce526bebf4fd51d237bcde4f5a2b
Joseph Ciurej <churayy@gmail.com>
2016-05-10 20:47:51 -0700

    Added some of the core files for the project infrastructure.

--- conf.lua
+++ conf.lua
@@ -1,9 +1,12 @@
 function love.conf( config )
   config.identity = "fxn"
-  config.version = "0.10.0"
+  config.version = "0.10.1"
   config.console = false
 
   config.window.title = "fxn"
+  config.window.icon = nil
+  config.window.borderless = false
+  config.window.resizable = false
   config.window.width = 640
   config.window.height = 480
   config.window.vsync = true
@@ -11,9 +14,11 @@ function love.conf( config )
   config.modules.audio = false
   config.modules.event = true
   config.modules.graphics = true
+  config.modules.math = true
   config.modules.physics = false
+  config.modules.sound = false
   config.modules.system = true
   config.modules.timer = true
   config.modules.window = true
-  config.modules.thread = true
+  config.modules.thread = false
 end
--- main.lua
+++ main.lua
@@ -2,36 +2,30 @@
 package.path = package.path .. ";fxn/?.lua"
 local fxn = require( "fxn" )
 
--- package.path = package.path .. ";debug/?.lua"
--- ldb = require( "debug.debugger" )
-
 function love.run()
   math.randomseed( os.time() )
 
   if love.load then love.load( arg ) end
   if love.timer then love.timer.step() end
 
-  local timedelta = 0
+  local dt = 0
   local isrunning = true
   while isrunning do
     if love.event then
       love.event.pump()
       for levent, a, b, c, d in love.event.poll() do
-        if levent == "quit" then
-          isrunning = false
-        end
-
+        if levent == "quit" then isrunning = false end
         love.handlers[levent]( a, b, c, d )
       end
     end
 
     if love.timer then
       love.timer.step()
-      timedelta = love.timer.getDelta()
+      dt = love.timer.getDelta()
     end
 
     if love.getinput then love.getinput() end
-    if love.update then love.update( timedelta ) end
+    if love.update then love.update( dt ) end
     if love.window and love.graphics and love.window.isCreated() then
       love.graphics.clear( 0, 0, 0 )
       love.draw()
@@ -50,7 +44,7 @@ function love.keypressed( key, scancode, isrepeat )
   if key == "q" then love.event.quit() end
 end
 
-function love.update( timedelta )
+function love.update( dt )
   
 end
 
