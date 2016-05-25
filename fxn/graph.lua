f2f288b56aa1139504af0800f3eceb6b6a3739da
Joseph Ciurej <churayy@gmail.com>
2016-05-24 22:37:35 -0700

    Adding the preliminary implementation for the 'graph' module.

--- fxn/graph.lua
+++ fxn/graph.lua
@@ -1,3 +1,174 @@
-local graph = {}
+local struct = require( 'struct' )
+local util = require( 'util' )
 
-return graph
+--[[ Constructor ]]--
+
+local graph_t = struct( {},
+  '_nodes', {},
+  '_edges', {labels={}, outgoing={}, incoming={}},
+  '_nextnid', 1
+)
+
+--[[ Public Functions ]]--
+
+function graph_t.addnode( self, nlabel )
+  local nid = self:_getnextnid()
+  local nlabel = nlabel == nil and true or nlabel
+
+  self._nodes[nid] = nlabel
+  self._edges.labels[nid] = {}
+  self._edges.outgoing[nid] = {}
+  self._edges.incoming[nid] = {}
+
+  return graph_t.node_t( self, nid )
+end
+
+-- TODO(JRC): Add an option for adding bidirectional edges.
+function graph_t.addedge( self, srcnode, dstnode, elabel )
+  if self:findnode( srcnode ) and self:findnode( dstnode ) then
+    if self:findedge( srcnode, dstnode ) then
+      self:removeedge( srcnode, dstnode )
+    end
+
+    local srcnid, dstnid = srcnode._nid, dstnode._nid
+    local elabel = elabel == nil and true or elabel
+    self._edges.labels[srcnid][dstnid] = elabel
+    self._edges.outgoing[srcnid][dstnid] = true
+    self._edges.incoming[dstnid][srcnid] = true
+
+    return graph_t.edge_t( self, srcnid, dstnid )
+  end
+end
+
+function graph_t.removenode( self, node )
+  local node = self:findnode( node )
+  if node then
+    for _, ie in ipairs( node:getinedges() ) do self:removeedge( ie ) end
+    for _, oe in ipairs( node:getoutedges() ) do self:removeedge( oe ) end
+
+    local nid = node._nid
+    self._nodes[nid] = nil
+    self._edges.labels[nid] = nil
+    self._edges.outgoing[nid] = nil
+    self._edges.incoming[nid] = nil
+  end
+end
+
+function graph_t.removeedge( self, ... )
+  local edge = self:findedge( ... )
+  if edge then
+    self._edges.labels[edge._srcnid][edge._dstnid] = nil
+    self._edges.outgoing[edge._srcnid][edge._dstnid] = nil
+    self._edges.incoming[edge._dstnid][edge._srcnid] = nil
+  end
+end
+
+function graph_t.findnode( self, node )
+  if self == node._graph and self._nodes[node._nid] ~= nil then
+    return node
+  end
+end
+
+function graph_t.findedge( self, ... )
+  local edge = nil
+
+  local arg = util.pack( ... )
+  if arg.n == 1 then edge = arg[1]
+  elseif arg.n == 2 then edge = graph_t.edge_t( self, arg[1]._nid, arg[2]._nid )
+  else edge = nil end
+
+  if edge and self == edge._graph and
+      self._edges.outgoing[edge._srcnid] ~= nil and 
+      self._edges.outgoing[edge._srcnid][edge._dstnid] ~= nil then
+    return edge
+  end
+end
+
+function graph_t.querynodes( self, queryfxn )
+  local queriednodes = {}
+  local queryfxn = queryfxn or function( v ) return true end
+
+  for nid, nlabel in pairs( self._nodes ) do
+    local node = graph_t.node_t( self, nid )
+    if queryfxn( node ) then table.insert( queriednodes, node ) end
+  end
+
+  return queriednodes
+end
+
+function graph_t.queryedges( self, queryfxn )
+  local queriededges = {}
+  local queryfxn = queryfxn or function( e ) return true end
+
+  for srcnid, srcverttoedges in pairs( self._edges.labels ) do
+    for dstnid, elabel in pairs( srcverttoedges ) do
+      local edge = graph_t.edge_t( self, srcnid, dstnid )
+      if queryfxn( edge ) then table.insert( queriededges, edge ) end
+    end
+  end
+
+  return queriededges
+end
+
+--[[ Private Functions ]]--
+
+function graph_t._getnextnid( self )
+  local nextnid = self._nextnid
+  self._nextnid = self._nextnid + 1
+  return nextnid
+end
+
+--[[ Private Classes ]]--
+
+graph_t.node_t = struct( {}, '_graph', nil, '_nid', -1 )
+
+function graph_t.node_t.__eq( self, other )
+  return self._graph == other._graph and self._nid == other._nid
+end
+
+function graph_t.node_t.getlabel( self )
+  return self._graph._nodes[self._nid] 
+end
+
+function graph_t.node_t.getoutedges( self )
+  local outedges = {}
+
+  for dstnid in pairs( self._graph._edges.outgoing[self._nid] ) do
+    local dstnode = graph_t.node_t( self._graph, dstnid )
+    table.insert( outedges, self._graph:findedge(self, dstnode) )
+  end
+
+  return outedges
+end
+
+function graph_t.node_t.getinedges( self )
+  local inedges = {}
+
+  for srcnid in pairs( self._graph._edges.incoming[self._nid] ) do
+    local srcnode = graph_t.node_t( self._graph, srcnid )
+    table.insert( inedges, self._graph:findedge(srcnode, self) )
+  end
+
+  return inedges
+end
+
+graph_t.edge_t = struct( {}, '_graph', nil, '_srcnid', -1, '_dstnid', -1 )
+
+function graph_t.edge_t.__eq( self, other )
+  return self._graph == other._graph and self._srcnid == other._srcnid and
+    self._dstnid == other._dstnid
+end
+
+function graph_t.edge_t.getlabel( self )
+  return self._graph._edges.labels[self._srcnid][self._dstnid]
+end
+
+function graph_t.edge_t.getsource( self )
+  return self._graph:findnode( graph_t.node_t(self._graph, self._srcnid) )
+end
+
+function graph_t.edge_t.getdestination( self )
+  return self._graph:findnode( graph_t.node_t(self._graph, self._dstnid) )
+end
+
+return graph_t
---
+++ spec/graph_spec.lua
@@ -0,0 +1,193 @@
+require( 'bustedext' )
+local graph_t = require( 'fxn.graph' )
+
+describe( 'graph', function()
+  --[[ Testing Variables ]]--
+
+  local testgraph = nil
+  local testnodes = nil
+  local testedges = nil
+  local testaltgraph = nil
+
+  --[[ Set Up / Tear Down Functions ]]--
+
+  before_each( function()
+    -- Test Graph Diagram:
+    --   +---->3-----+
+    --   |     |     |
+    --   |     |     v
+    --   2     |     5    1
+    --   |     |     ^
+    --   |     v     |
+    --   +---->4-----+
+    testgraph = graph_t()
+
+    testnodes = {}
+    for nodeidx = 1, 5, 1 do
+      local node = testgraph:addnode( tostring(nodeidx) )
+      table.insert( testnodes, node )
+    end
+
+    testedges = {}
+    local edgepairs = { {2, 3}, {2, 4}, {3, 5}, {4, 5}, {3, 4} }
+    for _, ep in ipairs( edgepairs ) do
+      local edge = testgraph:addedge( testnodes[ep[1]], testnodes[ep[2]],
+        tostring(ep[1]) .. '>' .. tostring(ep[2])  )
+      table.insert( testedges, edge )
+    end
+
+    -- Test AltGraph Diagram:
+    --
+    --   1---------->2
+    --
+    testaltgraph = graph_t()
+    testaltgraph:addedge( testaltgraph:addnode('1'),
+      testaltgraph:addnode('2'), '1>2' )
+  end )
+
+  --[[ Testing Functions ]]--
+
+  it( 'constructs instances that are initially empty', function()
+    local emptygraph = graph_t()
+
+    assert.are.equallists( {}, emptygraph:querynodes() )
+    assert.are.equallists( {}, emptygraph:queryedges() )
+  end )
+
+  --[[
+
+  it( 'properly adds nodes to the data structure', function()
+    assert.are.equivalentlists( testnodes, testgraph:querynodes() )
+
+    for nodeidx, node in ipairs( testnodes ) do
+      assert.is_true( vertex:isa(graph.Vertex) )
+      assert.are.equal( tostring(nodeidx), node:getlabel() )
+    end
+  end )
+
+  it( 'properly adds new edges to the data structure', function()
+    assert.are.equivalentlists( testedges, testgraph:queryedges() )
+
+    for edgeidx, edge in ipairs( testedges ) do
+      assert.is_true( edge:isa(graph.Edge) )
+      assert.are.equal(
+        edge:getsource():getlabel() .. '>' .. edge:getdestination():getlabel(),
+        edge:getlabel()
+      )
+    end
+  end )
+
+  it( 'does not allow adding edges with invalid start/end nodes', function()
+    local remotenodes = testaltgraph:querynodes()
+    assert.falsy( testgraph:addedge(testnodes[1], remotenodes[2]) )
+    assert.falsy( testgraph:addedge(remotenodes[1], testnodes[2]) )
+
+    assert.are.equivalentlists( testedges, testgraph:queryedges() )
+  end )
+
+  it( 'overwrites existing edges on edge readd', function()
+    local overwriteedge = testgraph:queryedges()[1]
+    local overwritesrc = overwriteedge:getsource()
+    local overwritedst = overwriteedge:getdestination()
+
+    local newelabel = '-' .. overwriteedge:getlabel() .. '-'
+    local newedge = testgraph:addedge( overwritesrc, overwritedst, newelabel )
+
+    assert.are.equal( newelabel, newedge:getlabel() )
+    assert.are.equal( overwritesrc, newedge:getsource() )
+    assert.are.equal( overwritedst, newedge:getdestination() )
+  end )
+
+  it( 'properly removes nodes from the data structure', function()
+    for testnodeidx = #testnodes, 1, -1 do 
+      local testnode = testnodes[testnodeidx]
+
+      table.remove( testnodes, testnodeidx )
+      testgraph:removenode( testnode )
+
+      assert.are.equivalentlists( testnodes, testgraph:querynodes() )
+    end
+  end )
+
+  it( 'removes all edges attached to a node upon its removal', function()
+    local edgegraph = graph()
+
+    local centralnode = edgegraph:addnode( 'c' )
+    local outernodes = {}
+    for vidx = 1, 6, 1 do
+      local outernode = edgegraph:addnode( tostring(vidx) )
+      table.insert( outernodes, outernode )
+
+      if vidx % 2 == 0 then edgegraph:addedge( centralnode, outernode )
+      else edgegraph:addedge( outernode, centralnode ) end
+    end
+
+    assert.are.equal( #outernodes, #edgegraph:queryedges() )
+    edgegraph:removenode( centralnode )
+    assert.are.equal( 0, #edgegraph:queryedges() )
+  end )
+
+  it( 'properly removes edges from the data structure', function()
+    for testedgeidx = #testedges, 1, -1 do 
+      local testedge = testedges[testedgeidx]
+
+      table.remove( testedges, testedgeidx )
+      testgraph:removeedge( testedge )
+
+      assert.are.equivalentlists( testedges, testgraph:queryedges() )
+    end
+  end )
+
+  it( 'returns only existing nodes queried via "findnode"', function()
+    for _, node in ipairs( testnodes ) do
+      assert.are.equal( node, testgraph:findnode(node) )
+    end
+
+    local remotenode = testaltgraph:querynodes()[1]
+    assert.falsy( testgraph:findnode(remotenode) )
+  end )
+
+  it( 'returns only existing edges queried via "findedge"', function()
+    for _, edge in ipairs( testedges ) do
+      assert.are.equal( edge, testgraph:findedge(edge) )
+    end
+
+    local remoteedge = testaltgraph:queryedges()[1]
+    assert.falsy( testgraph:findedge(remoteedge) )
+  end )
+
+  it( 'supports "findedge" edge queries using endpoint nodes', function()
+    for _, edge in ipairs( testedges ) do
+      local edgesrc = edge:getsource()
+      local edgedst = edge:getdestination()
+      assert.are.equal( edge, testgraph:findedge(edgesrc, edgedst) )
+    end
+
+    local remotenodes = testaltgraph:querynodes()
+    assert.falsy( testgraph:findedge(testnodes[1], testnodes[2]) )
+    assert.falsy( testgraph:findedge(testnodes[1], remotenodes[2]) )
+    assert.falsy( testgraph:findedge(remotenodes[1], remotenodes[2]) )
+  end )
+
+  it( 'facilitates arbitrary node queries with 'querynodes'', function()
+    local queriednodes = testgraph:querynodes( function(v)
+      return tonumber( v:getlabel() ) >= 3
+    end )
+
+    assert.are.equal( 3, #queriednodes )
+    assert.are.equivalentlists(
+      { testnodes[3], testnodes[4], testnodes[5] },
+      queriednodes
+    )
+  end )
+
+  it( 'facilitates arbitrary edge queries with "queryedges"', function()
+    local queriededges = testgraph:queryedges( function(e)
+      return e:getdestination() == testnodes[5] or e:getsource() == testnodes[5]
+    end )
+
+    assert.are.equal( 2, #queriededges )
+    assert.are.equivalentlists( {testedges[3], testedges[4]}, queriededges )
+  end )
+  ]]--
+end )
