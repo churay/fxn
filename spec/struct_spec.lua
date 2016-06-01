927be401bb068eea9e29071bafdfcb7fc3c9d192
Joseph Ciurej <churayy@gmail.com>
2016-05-31 23:13:42 -0700

    Updated and improved the organization of the 'struct' module tests.

--- spec/struct_spec.lua
+++ spec/struct_spec.lua
@@ -4,27 +4,15 @@ describe( 'struct', function()
   --[[ Testing Constants ]]--
 
   local BASE_VAL = 1
-  local OVER_VAL = 2
-
   local BASE_NAME = 'base'
-  local OVER_NAME = 'over'
-
-  local BASE_OBJ_TABLE = { BASE_VAL, BASE_NAME }
-  local OVER_OBJ_TABLE = { OVER_VAL, OVER_NAME }
-
+  local BASE_OTABLE = { BASE_VAL, BASE_NAME }
   local BASE_GETVAL = function( self ) return self.val end
-  local OVER_GETVAL = function( self ) return self.val end
-
   local BASE_GETNAME = function( self ) return BASE_NAME end
-  local OVER_GETNAME = function( self ) return OVER_NAME end
-
-  local BASE_SHR_TABLE = { BASE_VAL, BASE_NAME }
-  local OVER_SHR_TABLE = { OVER_VAL, OVER_NAME }
+  local BASE_STABLE = { BASE_VAL, BASE_NAME }
 
   --[[ Testing Variables ]]--
 
   local base_t = nil
-  local over_t = nil
 
   --[[ Testing Helper Functions ]]--
 
@@ -45,20 +33,15 @@ describe( 'struct', function()
   --[[ Set Up / Tear Down Functions ]]--
 
   before_each( function()
-    base_t = struct( {}, 'val', BASE_VAL, 'otable', BASE_OBJ_TABLE )
+    base_t = struct( {}, 'val', BASE_VAL, 'otable', BASE_OTABLE )
     base_t.getval = BASE_GETVAL
     base_t.getname = BASE_GETNAME
-    base_t.stable = BASE_SHR_TABLE
-
-    over_t = struct( {base_t}, 'val', OVER_VAL, 'otable', OVER_OBJ_TABLE )
-    over_t.getval = OVER_GETVAL
-    over_t.getname = OVER_GETNAME
-    over_t.stable = OVER_SHR_TABLE
+    base_t.stable = BASE_STABLE
   end )
 
   --[[ Testing Functions ]]--
 
-  describe( 'struct object creation and initialization functionality', function()
+  describe( 'struct object creation and field distribution functionality', function()
     local obj1, obj2 = nil, nil
 
     before_each( function()
@@ -91,8 +74,9 @@ describe( 'struct', function()
     end )
 
     it( 'distributes the same shared fields to all struct objects', function()
-      for _, sfield in ipairs( getobjfields(base_t) ) do
-        assert.are.equal( obj1[ofield], obj2[ofield] )
+      for _, sfield in ipairs( getshrfields(base_t) ) do
+        assert.are.equal( base_t[sfield], obj1[sfield] )
+        assert.are.equal( base_t[sfield], obj2[sfield] )
       end
     end )
 
@@ -113,56 +97,106 @@ describe( 'struct', function()
         assert.are.equal( base_t[sfield], obj2[sfield] )
       end
     end )
+  end )
+
+  describe( 'struct object set up and initialization functionality', function()
+    local OVER_VAL = BASE_VAL + 10
+    local OVER_NAME = BASE_NAME .. BASE_NAME
+    local OVER_OTABLE = { OVER_VAL, OVER_NAME }
+
+    it( 'supports default object value initialization to values specified in ' ..
+        'original struct value specification', function()
+      local defaultobj = base_t()
+      for _, ofield in ipairs( getobjfields(base_t) ) do
+        assert.are.same( base_t.__fields[ofield], defaultobj[ofield] )
+      end
+    end )
 
     it( 'supports object field overrides by giving values in struct call function ' ..
         '(w/ overrides occuring in struct field specification order)', function()
-      local OVER_BASE_VAL, OVER_OVER_VAL = BASE_VAL + 10, OVER_VAL + 20
-      local defs_t = struct( {}, 'base', BASE_VAL, 'over', OVER_VAL )
-      local defsobj = defs_t( OVER_BASE_VAL, OVER_OVER_VAL )
+      base_t._init = nil
+      local overobj = base_t( OVER_VAL, OVER_OTABLE )
 
-      assert.are.equal( BASE_VAL, defs_t.__fields.base )
-      assert.are.equal( OVER_VAL, defs_t.__fields.over )
+      assert.are.equal( BASE_VAL, base_t.__fields.val )
+      assert.are.equal( BASE_OTABLE, base_t.__fields.otable )
 
-      assert.are.equal( OVER_BASE_VAL, defsobj.base )
-      assert.are.equal( OVER_OVER_VAL, defsobj.over )
+      assert.are.equal( OVER_VAL, overobj.val )
+      assert.are.equal( OVER_OTABLE, overobj.otable )
     end )
 
     it( 'supports granular object field overrides and object initialization ' ..
         'through the definition of the "_init" function', function()
-      local OVER_BASE_VAL, OVER_OVER_VAL = BASE_VAL + 10, OVER_VAL + 20
-      local defs_t = struct( {}, 'base', BASE_VAL, 'over', OVER_VAL )
-      defs_t._init = function( self, overval, baseval )
-        self.base, self.over = baseval, overval
+      base_t._init = function( self, overotable, overval )
+        self.val, self.otable = overval, overotable
       end
-      local defsobj = defs_t( OVER_OVER_VAL, OVER_BASE_VAL )
+      local overobj = base_t( OVER_OTABLE, OVER_VAL )
 
-      assert.are.equal( BASE_VAL, defs_t.__fields.base )
-      assert.are.equal( OVER_VAL, defs_t.__fields.over )
+      assert.are.equal( BASE_VAL, base_t.__fields.val )
+      assert.are.equal( BASE_OTABLE, base_t.__fields.otable )
 
-      assert.are.equal( OVER_BASE_VAL, defsobj.base )
-      assert.are.equal( OVER_OVER_VAL, defsobj.over )
+      assert.are.equal( OVER_VAL, overobj.val )
+      assert.are.equal( OVER_OTABLE, overobj.otable )
     end )
   end )
 
-  --[[
   describe( 'struct field inheritance and overriding functionality', function()
-    it( 'distributes all nonoverridden fields to derived structs', function()
-      local desc_t = struct( {base_t} )
-      for _, field in ipairs( getfields(desc_t) ) do
-        assert.are.equal( base_t[field], desc_t[field] )
+    local OVER_VAL = 2
+    local OVER_GETVAL = function( self ) return self.val end
+    local OVER_STABLE = { OVER_VAL, OVER_NAME }
+
+    local over_t = nil
+    local baseobj, overobj = nil, nil
+
+    before_each( function()
+      over_t = struct( {base_t}, 'val', OVER_VAL )
+      over_t.getval = OVER_GETVAL
+      over_t.stable = OVER_STABLE
+
+      baseobj = base_t()
+      overobj = over_t()
+    end )
+
+    it( 'distributes all nonoverridden object fields to derived structs', function()
+      for _, ofield in ipairs( getobjfields(base_t) ) do
+        local isoverridden = false
+        for _, overofield in ipairs( getobjfields(over_t) ) do
+          isoverridden = isoverridden or ofield == overofield
+        end
+
+        if not isoverridden then
+          assert.are.same( base_t.__fields[ofield], overobj[ofield] )
+        end
       end
     end )
 
-    it( 'supports field overriding in derived structs', function()
-      for _, field in ipairs( getfields(over_t) ) do
-        assert.are_not.equal( base_t[field], over_t[field] )
-      end
+    it( 'distributes all nonoverridden shared fields to derived structs', function()
+      for _, sfield in ipairs( getshrfields(base_t) ) do
+        local isoverridden = false
+        for _, oversfield in ipairs( getshrfields(over_t) ) do
+          isoverridden = isoverridden or sfield == oversfield
+        end
 
-      assert.are.equal( OVER_VAL, overobj:getval() )
-      assert.are.equal( OVER_NAME, overobj:getname() )
+        if not isoverridden then
+          assert.are.equal( base_t[sfield], overobj[sfield] )
+        end
+      end
+    end )
+
+    it( 'supports object field overriding in derived structs', function()
+      for _, ofield in ipairs( getobjfields(over_t) ) do
+        assert.are.same( over_t.__fields[ofield], overobj[ofield] )
+      end
+    end )
+
+    it( 'supports shared field overriding in derived structs', function()
+      for _, sfield in ipairs( getshrfields(over_t) ) do
+        assert.are.equal( over_t[sfield], overobj[sfield] )
+      end
     end )
 
     it( 'supports field distribution from multiple specified base structs', function()
+      pending( 'TODO(JRC)' )
+      --[[
       local orth_t = struct( {}, 'data', OVER_VAL )
       orth_t.gettype = function( self ) return 'orth_t' end
       local multi_t = struct( {base_t, orth_t} )
@@ -173,10 +207,13 @@ describe( 'struct', function()
       for _, field in ipairs( getfields(orth_t) ) do
         assert.are.equal( orth_t[field], multi_t[field] )
       end
+      --]]
     end )
 
     it( 'distributes fields to derived structs with field priority being ' ..
         'taken in struct specification order', function()
+      pending( 'TODO(JRC)' )
+      --[[
       local basealt_t = struct( {}, 'val', OVER_VAL )
       basealt_t.gettype = function( self ) return 'basealt_t' end
       basealt_t.getname = function( self ) return OVER_NAME end
@@ -192,7 +229,7 @@ describe( 'struct', function()
           assert.are_not.equal( basealt_t[field], complex_t[field] )
         end
       end
+      --]]
     end )
   end )
-  ]]--
 end )
