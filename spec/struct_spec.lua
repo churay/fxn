57282ea9110309daf10f382511a23469dbab8229
Joseph Ciurej <churayy@gmail.com>
2016-05-31 20:26:47 -0700

    Implemented the override behavior test cases for the 'struct' module.

--- spec/struct_spec.lua
+++ spec/struct_spec.lua
@@ -9,6 +9,9 @@ describe( 'struct', function()
   local BASE_NAME = 'base'
   local OVER_NAME = 'over'
 
+  local BASE_TABLE = { BASE_VAL, BASE_NAME }
+  local OVER_TABLE = { OVER_VAL, OVER_NAME }
+
   --[[ Testing Variables ]]--
 
   local base_t = nil
@@ -31,13 +34,15 @@ describe( 'struct', function()
   --[[ Set Up / Tear Down Functions ]]--
 
   before_each( function()
-    base_t = struct( {}, 'val', BASE_VAL )
+    base_t = struct( {}, 'val', BASE_VAL, 'tbl', BASE_TABLE )
     base_t.getval = function( self ) return self.val end
     base_t.getname = function( self ) return BASE_NAME end
+    base_t.lcltbl = { BASE_VAL, BASE_NAME }
 
-    over_t = struct( {base_t}, 'val', OVER_VAL )
+    over_t = struct( {base_t}, 'val', OVER_VAL, 'tbl', OVER_TABLE )
     over_t.getval = function( self ) return self.val end
     over_t.getname = function( self ) return OVER_NAME end
+    over_t.lcltbl = { OVER_VAL, OVER_NAME }
 
     baseobj = base_t()
     overobj = over_t()
@@ -47,12 +52,15 @@ describe( 'struct', function()
 
   it( 'distributes all default struct fields to struct objects', function()
     assert.is.truthy( baseobj.val )
+    assert.is.truthy( baseobj.tbl )
     assert.are.equal( BASE_VAL, baseobj.val )
+    assert.are.equal( BASE_TABLE, baseobj.tbl )
   end )
 
   it( 'distributes all extra struct fields to struct objects', function()
     assert.is.truthy( baseobj.getval )
     assert.is.truthy( baseobj.getname )
+    assert.is.truthy( baseobj.lcltbl )
   end )
 
   it( 'distributes the same fields (default/extra) to all struct objects', function()
@@ -64,13 +72,27 @@ describe( 'struct', function()
   end )
 
   it( 'supports local default field overrides that do not impact other objects', function()
-    -- TODO(JRC): This test needs to test independence with nested table values.
-    pending( 'TODO(JRC)' )
+    local baseobj2 = base_t()
+
+    baseobj.val = OVER_VAL
+    assert.are_not.equal( base_t.val, baseobj.val )
+    assert.are.equal( base_t.val, baseobj2.val )
+
+    baseobj.tbl[1] = OVER_VAL
+    assert.are_not.same( base_t.tbl, baseobj.tbl )
+    assert.are.same( base_t.tbl, baseobj2.tbl )
   end )
 
   it( 'supports local extra field overrides that do not impact other objects', function()
-    -- TODO(JRC): This test needs to test independence with nested table values.
-    pending( 'TODO(JRC)' )
+    local baseobj2 = base_t()
+
+    baseobj.getval = function( self ) return self.val + 1 end
+    assert.are_not.equal( base_t.getval, baseobj.getval )
+    assert.are.equal( base_t.getval, baseobj2.getval )
+
+    baseobj.lcltbl[2] = BASE_NAME .. BASE_NAME
+    assert.are_not.same( base_t.lcltbl, baseobj.lcltbl )
+    assert.are.same( base_t.lcltbl, baseobj2.lcltbl )
   end )
 
   it( 'supports default field overrides in struct objects w/ overrides occuring ' ..
