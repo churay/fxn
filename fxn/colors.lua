c2731bcb3c84e8c7243f741f1dc7d9255c45c6b4
Joseph Ciurej <churayy@gmail.com>
2016-06-23 22:16:02 -0700

    Started implementing the basic logic for function rendering. Added a colors module to store all common colors used in the game.

---
+++ fxn/colors.lua
@@ -0,0 +1,6 @@
+local colors = {}
+
+colors.white = { 255, 255, 255 }
+colors.black = {   0,   0,   0 }
+
+return colors
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 local fxn = require( 'fxn' )
 -- local dbg = require( 'debugger' )
 
+local func = nil
+
 function love.run()
   math.randomseed( os.time() )
 
@@ -22,7 +24,8 @@ function love.run()
     if love.getinput then love.getinput() end
     if love.update then love.update( fxn.global.fdt + math.max(-frameleft, 0) ) end
     if love.window and love.graphics and love.window.isCreated() then
-      love.graphics.clear( 0, 0, 0 )
+      -- TODO(JRC): Change to clear color back to black after testing is complete.
+      love.graphics.clear( unpack(fxn.colors.white) )
       love.draw()
       love.graphics.present()
     end
@@ -37,7 +40,7 @@ function love.run()
 end
 
 function love.load()
-  
+  func = fxn.func_t( function(x) return math.sin(x) end )
 end
 
 function love.keypressed( key, scancode, isrepeat )
@@ -49,5 +52,21 @@ function love.update( dt )
 end
 
 function love.draw()
-  love.graphics.clear( 255, 255, 255 )
+  love.graphics.push()
+
+  do -- transform from device coordinates to window coordinates
+    love.graphics.scale( love.graphics.getDimensions() )
+    love.graphics.translate( 0.0, 1.0 )
+    love.graphics.scale( 1.0, -1.0 )
+  end
+
+  do -- plot example function
+    love.graphics.setColor( unpack(fxn.colors.black) )
+    love.graphics.setLineWidth( 0.01 )
+
+    love.graphics.translate( 0.0, 0.5 )
+    love.graphics.line( 0.01, 0.0, 0.99, 0.0 )
+  end
+
+  love.graphics.pop()
 end
