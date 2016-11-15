752bc205af2993ecb67042e5a4275a44428fcb2d
Joseph Ciurej <churayy@gmail.com>
2016-11-14 23:29:10 -0800

    Fixed the infinite loop bug in the 'table_t' implementation.

--- fxn/table_t.lua
+++ fxn/table_t.lua
@@ -1,21 +1,23 @@
-local struct = require( 'struct' )
 local util = require( 'util' )
 
 --[[ Constructor ]]--
 
-local table_t = struct( {}, '_default', nil )
+local table_t, table_mt = {}, {}
+
+function table_mt.__call( self, default )
+  local objtable = setmetatable( {}, table_t )
+  rawset( objtable, '__default', default )
+  return objtable
+end
 
 --[[ Operators ]]--
 
 function table_t.__index( self, key )
-  local defaultvalue = rawget( self, '_default' )
-  if defaultvalue ~= nil then
-    local value = util.copy( defaultvalue )
-    rawset( self, key, value )
-    return value
-  else
-    return nil
-  end
+  local value = util.copy( rawget(self, '__default') )
+  rawset( self, key, value )
+  return value
 end
 
-return table_t
+--[[ Public Functions ]]--
+
+return setmetatable( table_t, table_mt )
--- spec/table_spec.lua
+++ spec/table_spec.lua
@@ -19,7 +19,6 @@ describe( 'table_t', function()
   --[[ Set Up / Tear Down Functions ]]--
 
   before_each( function()
-    --[[
     testdeftable = table_t()
     testnumtable = table_t( DEFAULT_NUMBER )
     teststrtable = table_t( DEFAULT_STRING )
@@ -28,32 +27,27 @@ describe( 'table_t', function()
     for _, testtable in ipairs( testtables ) do
       testtable[TEST_KEY] = TEST_VALUE
     end
-    ]]--
   end )
 
   --[[ Testing Functions ]]--
 
   it( 'functions like a normal table when setting values', function()
-    pending( 'TODO(JRC)' )
     for _, testtable in ipairs( testtables ) do
       assert.are.equal( TEST_VALUE, rawget(testtable, TEST_KEY) )
     end
   end )
 
   it( 'functions like a normal table when accessing non-nil values', function()
-    pending( 'TODO(JRC)' )
     for _, testtable in ipairs( testtables ) do
       assert.are.equal( TEST_VALUE, testtable[TEST_KEY] )
     end
   end )
 
   it( 'returns nil for nil values when no default key value is set', function()
-    pending( 'TODO(JRC)' )
     assert.are.equal( nil, testdeftable['emptykey'] )
   end )
 
   it( 'returns the default value when a default key value is set', function()
-    pending( 'TODO(JRC)' )
     assert.are.equal( DEFAULT_NUMBER, testnumtable['emptykey'] )
     assert.are.equal( DEFAULT_STRING, teststrtable['emptykey'] )
   end )
