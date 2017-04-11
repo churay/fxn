77866e87f265c222a89aced856748cdb5db248a4
Joseph Ciurej <churayy@gmail.com>
2017-04-10 22:56:56 -0700

    Implemented a new lazy version of 'fxn.util.loadlib' (needs testing). Rearranged the contents of the 'fxn.util' module.

--- fxn/util.lua
+++ fxn/util.lua
@@ -1,5 +1,55 @@
 local util = {}
 
+--[[ Language Functions ]]--
+
+function util.pprint( o )
+  print( util.pretty(o) )
+end
+
+-- TODO(JRC): Update this function so that metatable values are printed.
+function util.pretty( o, noexpand, _currdepth )
+  local noexpand = noexpand or false
+  local _currdepth = _currdepth or 0
+  local padding = '  '
+
+  local prefix = string.rep( padding, _currdepth )
+  if type( o ) == 'string' then
+    return string.format( '%q', o )
+  elseif type( o ) == 'table' and not noexpand then
+    local elemstrs = {}
+    for k, v in pairs( o ) do
+      local kstr = util.pretty(k, true, 0)
+      local vstr = util.pretty(v, false, _currdepth + 1)
+      local elemstr = string.format( '%s%s[%s] = %s', prefix, padding, kstr, vstr )
+      table.insert( elemstrs, elemstr )
+    end
+
+    return string.format( '{%s%s}', ( #elemstrs ~= 0 and '\n' or ''),
+      table.concat(elemstrs, ',\n') )
+  else
+    return tostring( o )
+  end
+end
+
+function util.copy( orig, copymt, _copied )
+  if type( orig ) ~= 'table' then return orig end
+  if _copied and _copied[orig] then return _copied[orig] end
+
+  local copied = _copied or {}
+  local copy = copymt and setmetatable( {}, getmetatable(orig) ) or {}
+  copied[orig] = copy
+  for ok, ov in pairs( orig ) do
+    copy[util.copy(ok, copymt, copied)] = ( util.copy(ov, copymt, copied) )
+  end
+
+  return copy
+end
+
+function util.unpack( t, i )
+  local i = i or 1
+  if t[i] ~= nil then return t[i], util.unpack( t, i + 1 ) end
+end
+
 --[[ Mathematical Functions ]]--
 
 function util.inrange( v, min, max, exclusive )
@@ -16,28 +66,26 @@ function util.fequal( v1, v2, epsilon )
   return math.abs( v1 - v2 ) < epsilon
 end
 
---[[ System Functions ]]--
+--[[ String Functions ]]--
 
-function util.libload( libbase, debug )
-  local debug = debug or false
-  local lib = {}
+function util.istring( str, sep )
+  local sep = sep or '\n'
+  local pattern = string.format( '(.-)(%s)', sep )
 
-  local plibpaths = io.popen( 'ls -1a ' .. libbase )
-  for libpath in plibpaths:lines() do
-    if string.match( libpath, '^.*%.lua$' ) and libpath ~= 'init.lua' then
-      local libmodname = string.sub( libpath, 1, string.find(libpath, '%.lua$')-1 )
-      local libmodpath = string.format( '%s.%s', libbase, libmodname )
-      if debug then
-        local loadsuccess, loadresult = pcall( require, libmodpath )
-        if not loadsuccess then print( loadresult ) return nil end
-      else
-        lib[libmodname] = require( libmodpath )
-      end
+  local ipos, iidx = 1, 0
+  return function()
+    iidx = iidx + 1
+
+    local spos, epos, capture = string.find( str, pattern, ipos )
+    if spos then
+      ipos = epos + 1
+      return iidx, capture
+    elseif ipos <= #str then
+      capture = string.sub( str, ipos )
+      ipos = #str + 1
+      return iidx, capture
     end
   end
-  plibpaths:close()
-
-  return lib
 end
 
 --[[ Table Functions ]]--
@@ -96,54 +144,49 @@ function util.len( l )
   return len
 end
 
---[[ Language Functions ]]--
+--[[ System Functions ]]--
 
-function util.pprint( o )
-  print( util.pretty(o) )
-end
+function util.libload( libbase, debug )
+  local debug = debug or false
+  local lib, libmt = { __modules={} }, {}
 
--- TODO(JRC): Update this function so that metatable values are printed.
-function util.pretty( o, noexpand, _currdepth )
-  local noexpand = noexpand or false
-  local _currdepth = _currdepth or 0
-  local padding = '  '
+  -- TODO(JRC): Ammend this section so that it uses 'package.searchpath' to
+  -- determine whether the contents of the directory are valid or not.
+  local libpaths = io.popen( 'ls -1a ' .. libbase )
+  for libpath in libpaths:lines() do
+    if not string.match( libpath, '^%.+$' ) and libpath ~= 'init.lua' then
+      if os.execute( 'test -d ' .. libpath ) == 0 then
+        lib.__modules[libpath] = true
+      elseif string.match( libpath, '^.*%.lua$' ) then
+        local modname = string.sub( libpath, 1, string.find(libpath, '%.lua$')-1 )
+        lib.__modules[modname] = true
+      end
+    end
+  end
+  libpaths:close()
 
-  local prefix = string.rep( padding, _currdepth )
-  if type( o ) == 'string' then
-    return string.format( '%q', o )
-  elseif type( o ) == 'table' and not noexpand then
-    local elemstrs = {}
-    for k, v in pairs( o ) do
-      local kstr = util.pretty(k, true, 0)
-      local vstr = util.pretty(v, false, _currdepth + 1)
-      local elemstr = string.format( '%s%s[%s] = %s', prefix, padding, kstr, vstr )
-      table.insert( elemstrs, elemstr )
+  function libmt.__index( libtable, libpath )
+    local libenv, libenvpath = libtable, libbase
+    for partidx, libpathpart in util.istring( libpath, '%.' ) do
+      libenvpath = string.format( '%s.%s', libenvpath, libpathpart )
+
+      local libenvmods = rawget( libenv, '__modules' )
+      if libenvmods and libenvmods[libpathpart] then
+        if debug then
+          local loadsuccess, loadresult = pcall( require, libenvpath )
+          if not loadsuccess then print( loadresult ); return nil end
+        end
+        local libsubtable = require( libenvpath )
+        rawset( libenv, libpathpart, libsubtable )
+        libenv = libsubtable
+      elseif not libenvmods then libenv = libenv[libpathpart]
+      else libenv = nil end
     end
 
-    return string.format( '{%s%s}', ( #elemstrs ~= 0 and '\n' or ''),
-      table.concat(elemstrs, ',\n') )
-  else
-    return tostring( o )
-  end
-end
-
-function util.copy( orig, copymt, _copied )
-  if type( orig ) ~= 'table' then return orig end
-  if _copied and _copied[orig] then return _copied[orig] end
-
-  local copied = _copied or {}
-  local copy = copymt and setmetatable( {}, getmetatable(orig) ) or {}
-  copied[orig] = copy
-  for ok, ov in pairs( orig ) do
-    copy[util.copy(ok, copymt, copied)] = ( util.copy(ov, copymt, copied) )
+    return libenv
   end
 
-  return copy
-end
-
-function util.unpack( t, i )
-  local i = i or 1
-  if t[i] ~= nil then return t[i], util.unpack( t, i + 1 ) end
+  return setmetatable( lib, libmt )
 end
 
 return util
